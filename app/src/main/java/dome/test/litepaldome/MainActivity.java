c772ba6a41c5f3f3a5f21c05a146f38dd0a0b485
115MRLI <1159642490@qq.com>
2018-08-29 15:28:22 +0800

    注入LitePal数据库框架

---
+++ app/src/main/java/dome/test/litepaldome/MainActivity.java
@@ -0,0 +1,13 @@
+package dome.test.litepaldome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
